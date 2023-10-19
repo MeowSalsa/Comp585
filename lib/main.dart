562d22811326f5472f88c35ab4bed181426fe097
Alan Chavez <thealan663@gmail.com>
2023-10-18 18:10:32 -0700

    home buttons
    i created the home page buttons.
    and added a grid view with it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('Curved Square Icon Example'),
         ),
+        body: Center(
+          child:
+              CurvedSquareIcon(), // Replace with your custom curved square icon widget
+        ),
+      ),
+    );
+  }
+}
+
+class CurvedSquareIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Handle button tap action here
+      },
+      child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(
+                  20.0), // Adjust the radius for curved edges
+            ),
+            child: const Text("Weather Icon here"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Text('Weather Icon here'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Text('Weather Icon here'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Text('Weather Icon here'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Text('Weather Icon here'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: const Text('Weather Icon here'),
+          ),
+        ],
       ),
     );
   }
