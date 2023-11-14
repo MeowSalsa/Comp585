c2dd966f1047aa2d951194652ea12f1febf9e1d8
jcr62283 <jericho.raz.584@my.csun.edu>
2023-11-14 12:11:42 -0800

    Adjusted Current Weather Display Layout - Modified layout to be closer to planned layout   - Moved Location label into AppBar   - Adjusted size of Minor weather displays   - Current Weather Display is now scrollable

--- lib/weather_displays.dart
+++ lib/weather_displays.dart
@@ -2,32 +2,69 @@ import 'package:flutter/material.dart';
 
 class MinorWeatherDisplay extends StatelessWidget {
 
+  final String? titleText;
   final String? displayText;
 
   const MinorWeatherDisplay({
     super.key,
+    this.titleText,
     this.displayText,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(30.0),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
+        borderRadius: BorderRadius.circular(40),
         child: Container(
+          padding: const EdgeInsets.all(15.0),
           color: const Color(0x80E7E7E7),
-          child: Center(
-            child: Text(
-              "$displayText"
-            ),
-          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Text(
+                "$titleText",
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        "$displayText"
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ]
+          )
         ),
       ),
     );
   }
 }
 
-class WindDisplay extends MinorWeatherDisplay {
+class WindDisplay extends StatelessWidget {
+  
+  final String? windSpeed;
+  final String? windDirection;
 
+  const WindDisplay({
+    super.key,
+    required this.windSpeed,
+    required this.windDirection
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MinorWeatherDisplay(titleText: "WIND", displayText: "$windSpeed $windDirection",);
+  }
 }
