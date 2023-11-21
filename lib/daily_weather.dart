dcc7d7e623a9e27b1639173b9188940da1158292
jcr62283 <jericho.raz.584@my.csun.edu>
2023-11-20 18:19:06 -0800

    Linked Home Screen to Current Weather Display - Selecting a widget from the Home Screen leads to Weather Display - Localized Text Theme to Current Weather Display - Functionality of both Screens are Unchanged

--- lib/daily_weather.dart
+++ lib/daily_weather.dart
@@ -30,97 +30,105 @@ class CurrentWeatherDisplay extends StatelessWidget {
           return const CircularProgressIndicator();
         }
 
+        final TextTheme textTheme = Theme.of(context).textTheme.apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        );
+
         final TimeBasedColorScheme colorScheme = TimeBasedColorScheme.colorSchemeFromLocalTime(LocalTime.getLocalDayPercent(weatherData.locationLongitude));
 
-        return Scaffold(
-          
-          body: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  colorScheme.skyStartColor,
-                  colorScheme.skyEndColor,
-                ],
-                begin: Alignment.topCenter,
-                end: const Alignment(0.0, 0.75),
-                stops: const [0.0, 1.0],
-                tileMode: TileMode.clamp
+        return Theme(
+          data: Theme.of(context).copyWith(textTheme: textTheme),
+          child: Scaffold(
+            
+            body: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    colorScheme.skyStartColor,
+                    colorScheme.skyEndColor,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: const Alignment(0.0, 0.75),
+                  stops: const [0.0, 1.0],
+                  tileMode: TileMode.clamp
+                ),
               ),
-            ),
-            child: Center(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
 
-                  // Location label
-                  Padding(
-                    padding: EdgeInsets.only(left: screenWidth / 24.0, top: screenWidth / 24.0),
+                    // Location label
+                    Padding(
+                      padding: EdgeInsets.only(left: screenWidth / 24.0, top: screenWidth / 24.0),
 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
 
-                        Flexible(
-                          child: FittedBox(
-                            fit: BoxFit.scaleDown,
-                            clipBehavior: Clip.hardEdge,
-                            child: Text(
-                              "${weatherData.currentCity}, ${weatherData.currentState}",
-                              maxLines: 1,
-                              style: TextStyle(
-                                fontSize: screenWidth / 20.0,
-                                fontWeight: FontWeight.bold,
+                          Flexible(
+                            child: FittedBox(
+                              fit: BoxFit.scaleDown,
+                              clipBehavior: Clip.hardEdge,
+                              child: Text(
+                                "${weatherData.currentCity}, ${weatherData.currentState}",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontSize: screenWidth / 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
 
-                        // MAJOR WEATHER DISPLAY
-                        MajorWeatherDisplay(
-                          temperatureLabel: "${weatherData.currentTemp}\u00B0${weatherData.currentUnits}", 
-                          longitude: weatherData.locationLongitude,
-                          conditionLabel: "${weatherData.currentCond}",
-                        ),
-
-                        Padding(padding: EdgeInsets.only(top: screenWidth * (3.0 / 8.0))),
-
-                        // MINOR WEATHER DISPLAYS
-                        Card(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero,
+                          // MAJOR WEATHER DISPLAY
+                          MajorWeatherDisplay(
+                            temperatureLabel: "${weatherData.currentTemp}\u00B0${weatherData.currentUnits}", 
+                            longitude: weatherData.locationLongitude,
+                            conditionLabel: "${weatherData.currentCond}",
                           ),
-                          color: colorScheme.mainBGColor,
-                          margin: EdgeInsets.zero,
-                          child: Column(
-                            children: [
-                              GridView(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 1, crossAxisCount: 2),
 
-                                children: [
-                                  WindDisplay(windSpeed: weatherData.currentWindSpeed, windDirection: weatherData.currentWindDirection,),
-                                  PrecipitationDisplay(precipitationChance: weatherData.currentPrecipitationChance,),
-                                  HumidityDisplay(humidityPercent: weatherData.currentHumidity,),
-                                ],
-                              ),
+                          Padding(padding: EdgeInsets.only(top: screenWidth * (3.0 / 8.0))),
 
-                              // Placeholder for buttons
-                              const Text("hello",),
-                            ],
+                          // MINOR WEATHER DISPLAYS
+                          Card(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
+                            color: colorScheme.mainBGColor,
+                            margin: EdgeInsets.zero,
+                            child: Column(
+                              children: [
+                                GridView(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(childAspectRatio: 1, crossAxisCount: 2),
+
+                                  children: [
+                                    WindDisplay(windSpeed: weatherData.currentWindSpeed, windDirection: weatherData.currentWindDirection,),
+                                    PrecipitationDisplay(precipitationChance: weatherData.currentPrecipitationChance,),
+                                    HumidityDisplay(humidityPercent: weatherData.currentHumidity,),
+                                  ],
+                                ),
+
+                                // Placeholder for buttons
+                                const Text("hello",),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
